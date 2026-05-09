eb6a9682ce13b8f9120c3d315c7ec518c7c2e3e4
Chandupa Marapana <136840067+chandupamarapana@users.noreply.github.com>
2026-05-09 14:35:56 +0530

    generated the spring folder and dependencies

---
+++ logmaster-api/src/main/java/com/logmaster/api/LogmasterApiApplication.java
@@ -0,0 +1,13 @@
+package com.logmaster.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogmasterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogmasterApiApplication.class, args);
+	}
+
+}
---
+++ logmaster-api/src/test/java/com/logmaster/api/LogmasterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logmaster.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogmasterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
