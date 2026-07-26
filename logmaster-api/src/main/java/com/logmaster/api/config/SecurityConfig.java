be65ecea4a0ad2ae39cb4142100a5a3cb47ece12
Chandupa Marapana <136840067+chandupamarapana@users.noreply.github.com>
2026-07-26 19:56:41 +0530

    chore: replace the link with the actual vercell link

--- logmaster-api/src/main/java/com/logmaster/api/config/SecurityConfig.java
+++ logmaster-api/src/main/java/com/logmaster/api/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOriginPatterns(List.of(
                 "http://localhost:5173",
-                "https://*.vercel.app"
+                "https://logmaster.vercel.app"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
